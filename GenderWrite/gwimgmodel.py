ee0a8fcf4a042fdae05511c42f89098a6e79093b
nicodv <njdevos@gmail.com>
2013-04-09 16:44:24 +0300

    Update and rename gwmaxout.py to gwimgmodel.py

---
+++ GenderWrite/gwimgmodel.py
@@ -0,0 +1,51 @@
+import numpy as np
+from pylearn2.utils import serial
+import theano.tensor as T
+from theano import function
+
+DATA_DIR = '/home/nico/Code/kaggle/GenderWrite/'
+
+# load configuration file and train model
+train_obj = serial.load_train_file(DATA_DIR+'gwmaxout.yaml'
+train_obj.main_loop()
+
+# generate model output
+def get_output(model, tdata, layerindex=-1, batch_size=100):
+    Xb = model.get_input_space().make_batch_theano()
+    ymf = model.fprop(Xb, return_all=True)
+    
+    data = tdata.get_topological_view()
+    
+    propagate = function([Xb],ymf)
+    
+    output = []
+    for ii in xrange(int(data.shape[0]/batch_size)):
+        seldata = data[ii*batch_size:(ii+1)*batch_size,:]
+        output.append(propagate(seldata)[layerindex])
+    
+    output = np.reshape(output,[data.shape[0],-1])
+    
+    if extralength < batch_size:
+        # remove the filler
+        output = output[:-extralength]
+    
+    return output
+
+trainset = serial.load(DATA_DIR+'gw_preprocessed_tottrain.pkl')
+testset = serial.load(DATA_DIR+'gw_preprocessed_test.pkl')
+
+outtrainset = get_output(model,trainset,-1)
+outtest = get_output(model,testset,-1)
+# save test output as submission
+np.savetxt(DATA_DIR+'model_comb.csv', outtestset[:,0], delimiter=",")
+
+# construct data sets with model output
+outtrainset = get_output(model,trainset,-2)
+outtestset = get_output(model,testset,-2)
+        
+# reshape model output so that shape[0] = no. of pages
+
+
+# save
+np.save(DATA_DIR+'maxout_train', outtrainset)
+np.save(DATA_DIR+'maxout_test', outtestset)
