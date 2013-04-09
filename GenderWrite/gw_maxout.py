1f8b79a953901e6c362b2f65302f4beba23fe695
nicodv <njdevos@gmail.com>
2013-04-09 14:41:08 +0300

    Update gw_maxout.py

--- GenderWrite/gw_maxout.py
+++ GenderWrite/gw_maxout.py
@@ -1,9 +1,10 @@
-import os
 import numpy as np
 from pylearn2.utils import serial
 
+DATA_DIR = '/home/nico/Code/kaggle/GenderWrite/'
+
 # load configuration file and train model
-train_obj = serial.load_train_file('/home/nico/Code/kaggle/GenderWrite/gw_maxout.yaml'
+train_obj = serial.load_train_file(DATA_DIR+'gw_maxout.yaml'
 train_obj.main_loop()
 
 # generate model output
@@ -11,3 +12,5 @@ train_obj.main_loop()
 # reshape model output so that shape[0] = no. of pages
 
 # save
+np.save(DATA_DIR+'maxout_train.npy', trainout)
+np.save(DATA_DIR+'maxout_test.npy', testout)
