1acf6e13eff0b5dafa9572c59571fafd7733c54e
nicodv <njdevos@gmail.com>
2013-04-09 14:14:45 +0300

    Update gwhybrid.py

--- GenderWrite/gwhybrid.py
+++ GenderWrite/gwhybrid.py
@@ -9,18 +9,19 @@ DATA_DIR = '/home/nico/datasets/Kaggle/GenderWrite/'
 def load_data():
     mtrain = np.load(DATA_DIR+'maxout_train.npy')
     mtest = np.load(DATA_DIR+'maxout_test.npy')
-
+    
     feattrain = np.load(DATA_DIR+'feat_train.npy')
     feattest = np.load(DATA_DIR+'feat_test.npy')
     
     traindata = np.concatenate((mtrain, feattrain), axis=1)
     testdata = np.concatenate((mtest, feattest), axis=1)
     
+    targets_pp = np.load(DATA_DIR+'targets_per_page.npy')[:,0]
     targets = np.load(DATA_DIR+'targets.npy')[:,0]
     
-    return traindata, testdata, targets
+    return traindata, testdata, targets_pp, targets
 
-def train_model(traindata, targets):
+def train_comb_model(traindata, targets):
     
     models = [
                 ensemble.GradientBoostingClassifier(n_estimators=500, learning_rate=0.05, \
@@ -36,7 +37,7 @@ def train_model(traindata, targets):
     for i in range(len(models)):
         # get scores
         scores[i] = cross_validation.cross_val_score(models[i], traindata, targets, \
-                    cv=cv, n_jobs=-1, score_func=metrics.auc_score)
+                    cv=cv, n_jobs=-1, score_func=metrics.accuracy_score)
         print "Cross-validation accuracy on the training set for model %d:" % i
         print "%0.3f (+/-%0.03f)" % (scores[i].mean(), scores[i].std() / 2)
         
@@ -44,12 +45,23 @@ def train_model(traindata, targets):
     
     return models
 
+def_train_final_model(traindata, targets):
+    
+    model = linear_model.LogisticRegression(penalty='l2', dual=True, C=1)
+    model.fit(traindata, targets)
+    
+
 if __name__ == '__main__':
     
-    traindata, testdata, targets = load_data()
+    traindata, testdata, targets_pp, targets = load_data()
     
-    models = train_model(traindata, targets)
+    # this model uses all features to predict gender per page
+    models = train_comb_model(traindata, targets_pp)
+    output_comb = models[0].predict_proba(testdata)
     
-    output = models[0].predict_proba(testdata)
-    np.savetxt(DATA_DIR+'model_hybrid.csv', output[:,0], delimiter=",")
+    # this model combines gender predictions per page into prediction per writer
+    fmodel = train_final_model(output_comb, targets)
+    output_comb = fmodel.predict_proba(testdata)
+    
+    np.savetxt(DATA_DIR+'submission.csv', output_comb[:,0], delimiter=",")
     
