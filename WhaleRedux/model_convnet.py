ce8381d12bf0b90110420c33c18fa77fd5a9f04e
nicodv <njdevos@gmail.com>
2013-05-30 10:56:17 +0300

    Update model_convnet.py
    updated dropout

--- WhaleRedux/model_convnet.py
+++ WhaleRedux/model_convnet.py
@@ -5,22 +5,19 @@ from theano import function
 import WhaleRedux.whaledata
 
 from pylearn2.train import Train
-from pylearn2.models.mlp import MLP, ConvRectifiedLinear, Softmax, Linear, Sigmoid, RectifiedLinear
+from pylearn2.models.mlp import MLP, ConvRectifiedLinear, Softmax, RectifiedLinear
 from pylearn2.costs.mlp.dropout import Dropout
 from pylearn2.space import Conv2DSpace
 from pylearn2.training_algorithms.sgd import SGD, ExponentialDecay, MomentumAdjustor
 from pylearn2.termination_criteria import EpochCounter
-from pylearn2.costs.cost import MethodCost
 from sklearn.metrics.metrics import auc_score
 
 DATA_DIR = '/home/nico/datasets/Kaggle/WhaleRedux/'
 
-def get_conv2D(dim_input):
+def get_conv2D(dim_input, batch_size=200):
     config = {
-        'batch_size': 200,
+        'batch_size': batch_size,
         'input_space': Conv2DSpace(shape=dim_input[:2], num_channels=dim_input[2]),
-        'dropout_include_probs': [1, 1, 1, 0.5, 1],
-        'dropout_input_include_prob': 0.8,
         'layers': [
         ConvRectifiedLinear(layer_name='h0', output_channels=40, irange=.04, init_bias=0.5, max_kernel_norm=1.9365,
             kernel_shape=[7, 7], pool_shape=[6, 4], pool_stride=[3, 2], W_lr_scale=0.64),
@@ -35,12 +32,10 @@ def get_conv2D(dim_input):
     }
     return MLP(**config)
 
-def get_conv1D(dim_input):
+def get_conv1D(dim_input, batch_size=200):
     config = {
-        'batch_size': 200,
+        'batch_size': batch_size,
         'input_space': Conv2DSpace(shape=dim_input[:2], num_channels=dim_input[2]),
-        'dropout_include_probs': [1, 1, 1, 0.5, 1],
-        'dropout_input_include_prob': 0.8,
         'layers': [
         ConvRectifiedLinear(layer_name='h0', output_channels=40, irange=.04, init_bias=0.5, max_kernel_norm=1.9365,
             kernel_shape=[7, 1], pool_shape=[4, 1], pool_stride=[3, 1], W_lr_scale=0.64),
@@ -55,15 +50,17 @@ def get_conv1D(dim_input):
     }
     return MLP(**config)
 
-def get_trainer(model, trainset, validset, epochs=50):
-    monitoring_batches = None if validset is None else 50
+def get_trainer(model, trainset, validset, epochs=50, batch_size=200):
+    monitoring_batches = None if validset is None else 40
     train_algo = SGD(
-        batch_size = 200,
+        batch_size = batch_size,
         init_momentum = 0.5,
         learning_rate = 0.5,
         monitoring_batches = monitoring_batches,
         monitoring_dataset = validset,
-        cost = Dropout(),
+        cost = Dropout(input_include_probs={'h0': 0.8, 'h1': 1., 'h2': 1., 'h3': 1., 'h4': 0.5, 'h5': 1.},
+                        input_scales={'h0': 1./0.8, 'h1': 1./1., 'h2': 1./1., 'h3': 1./1., 'h4': 1./0.5, 'h5': 1./1.},
+                        default_input_include_prob=0.5, default_input_scale=1./0.5),
         termination_criterion = EpochCounter(epochs),
         update_callbacks = ExponentialDecay(decay_factor=1.0005, min_lr=0.001)
     )
@@ -102,6 +99,7 @@ def get_output(model, tdata, layerindex, batch_size=200):
 if __name__ == '__main__':
     
     submission = False
+    batch_size = 200
     
     ####################
     #   MEL SPECTRUM   #
@@ -109,8 +107,8 @@ if __name__ == '__main__':
     trainset,validset,testset = WhaleRedux.whaledata.get_dataset('melspectrum', tot=submission)
     
     # build and train classifiers for submodels
-    model = get_conv2D([67,40,1])
-    get_trainer(model, trainset, validset, 80).main_loop()
+    model = get_conv2D([67,40,1], batch_size=batch_size)
+    get_trainer(model, trainset, validset, epochs=80, batch_size=batch_size).main_loop()
     
     # validate model
     if not submission:
@@ -137,8 +135,8 @@ if __name__ == '__main__':
     trainset2,validset2,testset2 = WhaleRedux.whaledata.get_dataset('specfeat', tot=submission)
     
     # build and train classifiers for submodels
-    model2 = get_conv1D([67,1,24])
-    get_trainer(model2, trainset2, validset2, 40).main_loop()
+    model2 = get_conv1D([67,1,24], batch_size=batch_size)
+    get_trainer(model2, trainset2, validset2, epochs=40, batch_size=batch_size).main_loop()
     
     # validate model
     if not submission:
