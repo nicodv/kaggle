4016f6de3a9f117eb3c35f31e832916051deb57b
nicodv <njdevos@gmail.com>
2013-07-19 12:21:52 +0200

    Update kmodes.py

--- Employee/kmodes.py
+++ Employee/kmodes.py
@@ -1,70 +1,36 @@
 #!/usr/bin/env python
-# kmeans.py using any of the 20-odd metrics in scipy.spatial.distance
-# kmeanssample 2 pass, first sample sqrt(N)
 
-from __future__ import division
 import random
 import numpy as np
-from scipy.spatial.distance import cdist  # $scipy/spatial/distance.py
-    # http://docs.scipy.org/doc/scipy/reference/spatial.html
-from scipy.sparse import issparse  # $scipy/sparse/csr.py
+from scipy.sparse import issparse
 
-__date__ = "2011-11-17 Nov denis"
-    # X sparse, any cdist metric: real app ?
-    # centres get dense rapidly, metrics in high dim hit distance whiteout
-    # vs unsupervised / semi-supervised svm
-
-#...............................................................................
-def kmodes( X, centres, delta=.001, maxiter=10, metric="euclidean", p=2, verbose=1 ):
-    """ centres, Xtocentre, distances = kmeans( X, initial centres ... )
-    in:
-        X N x dim  may be sparse
-        centres k x dim: initial centres, e.g. random.sample( X, k )
-        delta: relative error, iterate until the average distance to centres
-            is within delta of the previous average distance
-        maxiter
-        metric: any of the 20-odd in scipy.spatial.distance
-            "chebyshev" = max, "cityblock" = L1, "minkowski" with p=
-            or a function( Xvec, centrevec ), e.g. Lqmetric below
-        p: for minkowski metric -- local mod cdist for 0 < p < 1 too
-        verbose: 0 silent, 2 prints running distances
-    out:
+def kmodes(X, centroids, maxiter=10):
+    '''out:
         centres, k x dim
         Xtocentre: each X -> its nearest centre, ints N -> k
         distances, N
-    see also: kmeanssample below, class Kmeans below.
-    """
-    if not issparse(X):
-        X = np.asanyarray(X)  # ?
-    centres = centres.todense() if issparse(centres) \
-        else centres.copy()
+    '''
+    
     N, dim = X.shape
-    k, cdim = centres.shape
-    if dim != cdim:
-        raise ValueError( "kmeans: X %s and centres %s must have the same number of columns" % (
-            X.shape, centres.shape ))
-    if verbose:
-        print "kmeans: X %s  centres %s  delta=%.2g  maxiter=%d  metric=%s" % (
-            X.shape, centres.shape, delta, maxiter, metric)
+    k, cdim = centroids.shape
+    assert dim == cdim
+    
     allx = np.arange(N)
-    prevdist = 0
     for jiter in range( 1, maxiter+1 ):
-        D = cdist_sparse( X, centres, metric=metric, p=p )  # |X| x |centres|
-        xtoc = D.argmin(axis=1)  # X -> nearest centre
-        distances = D[allx,xtoc]
-        avdist = distances.mean()  # median ?
-        if verbose >= 2:
-            print "kmeans: av |X - nearest centre| = %.4g" % avdist
-        if (1 - delta) * prevdist <= avdist <= prevdist \
-        or jiter == maxiter:
+        D = get_distances(X, centroids)
+        xtoc = D.argmin(axis=1)
+        Xdistances = D[allx,xtoc]
+        print "Median distance X to nearest centroid = %.4g" % Xdistances.median()
+        if jiter == maxiter:
             break
-        prevdist = avdist
-        for jc in range(k):  # (1 pass in C)
-            c = np.where( xtoc == jc )[0]
+        for jc in range(k):
+            c = np.where(xtoc==jc)[0]
             if len(c) > 0:
-                centres[jc] = X[c].mean( axis=0 )
-    if verbose:
-        print "kmeans: %d iterations  cluster sizes:" % jiter, np.bincount(xtoc)
+                nc = X[c].count_nonzero(axis=0)
+                nc = X[xtoc].count_nonzero(axis=0)
+                n = len(X)
+                centroids[jc] = ( (nq/n) >= (nc/n) )
+    print("%d iterations,  cluster sizes:" % jiter, np.bincount(xtoc))
     if verbose >= 2:
         r50 = np.zeros(k)
         r90 = np.zeros(k)
@@ -72,17 +38,17 @@ def kmodes( X, centres, delta=.001, maxiter=10, metric="euclidean", p=2, verbose
             dist = distances[ xtoc == j ]
             if len(dist) > 0:
                 r50[j], r90[j] = np.percentile( dist, (50, 90) )
-        print "kmeans: cluster 50 % radius", r50.astype(int)
-        print "kmeans: cluster 90 % radius", r90.astype(int)
+        print "Cluster 50% radius", r50.astype(int)
+        print "Cluster 90% radius", r90.astype(int)
             # scale L1 / dim, L2 / sqrt(dim) ?
-    return centres, xtoc, distances
+    return centroids, xtoc, distances
 
 #...............................................................................
-def kmeanssample( X, k, nsample=0, **kwargs ):
-    """ 2-pass kmeans, fast for large N:
+def kmodesinit( X, k, nsample=0, **kwargs ):
+    ''' 2-pass kmeans, fast for large N:
         1) kmeans a random sample of nsample ~ sqrt(N) from X
         2) full kmeans, starting from those centres
-    """
+    '''
         # merge w kmeans ? mttiw
         # v large N: sample N^1/2, N^1/2 of that
         # seed like sklearn ?
@@ -94,39 +60,25 @@ def kmeanssample( X, k, nsample=0, **kwargs ):
     samplecentres = kmeans( Xsample, pass1centres, **kwargs )[0]
     return kmeans( X, samplecentres, **kwargs )
 
-def cdist_sparse( X, Y, **kwargs ):
-    """ -> |X| x |Y| cdist array, any cdist metric
-        X or Y may be sparse -- best csr
-    """
-        # todense row at a time, v slow if both v sparse
-    sxy = 2*issparse(X) + issparse(Y)
-    if sxy == 0:
-        return cdist( X, Y, **kwargs )
-    d = np.empty( (X.shape[0], Y.shape[0]), np.float64 )
-    if sxy == 2:
-        for j, x in enumerate(X):
-            d[j] = cdist( x.todense(), Y, **kwargs ) [0]
-    elif sxy == 1:
-        for k, y in enumerate(Y):
-            d[:,k] = cdist( X, y.todense(), **kwargs ) [0]
-    else:
-        for j, x in enumerate(X):
-            for k, y in enumerate(Y):
-                d[j,k] = cdist( x.todense(), y.todense(), **kwargs ) [0]
-    return d
+def get_distances(X, C):
+    D = np.array(X.shape[0], C.shape[0])
+    for ii in X.shape[0]:
+        for jj in C.shape[0]:
+            D[ii,jj] = (X[ii,:] != C[jj,:]).sum()
+    return D
 
 def randomsample( X, n ):
-    """ random.sample of the rows of X
+    ''' random.sample of the rows of X
         X may be sparse -- best csr
-    """
+    '''
     sampleix = random.sample( xrange( X.shape[0] ), int(n) )
     return X[sampleix]
 
 def nearestcentres( X, centres, metric="euclidean", p=2 ):
-    """ each X -> nearest centre, any metric
+    ''' each X -> nearest centre, any metric
             euclidean2 (~ withinss) is more sensitive to outliers,
             cityblock (manhattan, L1) less sensitive
-    """
+    '''
     D = cdist( X, centres, metric=metric, p=p )  # |X| x |centres|
     return D.argmin(axis=1)
 
@@ -136,28 +88,28 @@ def Lqmetric( x, y=None, q=.5 ):
         else (np.abs(x) ** q) .mean()
 
 #...............................................................................
-class Kmeans:
-    """ km = Kmeans( X, k= or centres=, ... )
-        in: either initial centres= for kmeans
-            or k= [nsample=] for kmeanssample
-        out: km.centres, km.Xtocentre, km.distances
+class Kmodes:
+    ''' km = Kmodes( X, k= or centroids=, ... )
+        in: either initial centroids= for kmodes
+            or k= [nsample=] for kmodesinit
+        out: km.centroids, km.Xtocentre, km.distances
         iterator:
-            for jcentre, J in km:
-                clustercentre = centres[jcentre]
+            for jcentroid, J in km:
+                clustercentroid = centroids[jcentroid]
                 J indexes e.g. X[J], classes[J]
-    """
-    def __init__( self, X, k=0, centres=None, nsample=0, **kwargs ):
+    '''
+    def __init__( self, X, k=0, centroids=None, nsample=0, **kwargs ):
         self.X = X
-        if centres is None:
-            self.centres, self.Xtocentre, self.distances = kmeanssample(
+        if centroids is None:
+            self.centroids, self.Xtocentroid, self.distances = kmodesinit(
                 X, k=k, nsample=nsample, **kwargs )
         else:
-            self.centres, self.Xtocentre, self.distances = kmeans(
-                X, centres, **kwargs )
+            self.centroids, self.Xtocentroid, self.distances = kmodes(
+                X, centroids, **kwargs )
 
     def __iter__(self):
-        for jc in range(len(self.centres)):
-            yield jc, (self.Xtocentre == jc)
+        for jc in range(len(self.centroids)):
+            yield jc, (self.Xtocentroid == jc)
 
 #...............................................................................
 if __name__ == "__main__":
