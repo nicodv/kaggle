d27d73912d3a70e170e5a0712f1788e3a2661d80
nicodv <njdevos@gmail.com>
2013-07-19 17:27:51 +0200

    Update kmodes.py

--- Employee/kmodes.py
+++ Employee/kmodes.py
@@ -1,147 +1,101 @@
 #!/usr/bin/env python
 
+'''
+Simple implementation of the k-modes algorithm by Huang [1997, 1998]
+
+N.J. de Vos
+'''
+
 import random
 import numpy as np
-from scipy.sparse import issparse
 
-def kmodes(X, centroids, maxiter=10):
-    '''out:
-        centres, k x dim
-        Xtocentre: each X -> its nearest centre, ints N -> k
-        distances, N
-    '''
+def kmodes(X, k, maxiters=10):
+    # input: data (no. points * no. attributes), number of clusters
+    # returns: cluster numbers for elements of X, centroids
     
+    # convert to numpy array, if needed
+    X = np.asanyarray(X)
     N, dim = X.shape
-    k, cdim = centroids.shape
-    assert dim == cdim
+    cent = np.array(k, dim)
     
-    allx = np.arange(N)
-    for jiter in range( 1, maxiter+1 ):
-        D = get_distances(X, centroids)
-        xtoc = D.argmin(axis=1)
-        Xdistances = D[allx,xtoc]
-        print "Median distance X to nearest centroid = %.4g" % Xdistances.median()
-        if jiter == maxiter:
-            break
-        for jc in range(k):
-            # indices of points in current cluster
-            c = np.where(xtoc==jc)[0]
-            if len(c) > 0:
-                nq = X[c].count_nonzero(axis=0)
-                nc = X[xtoc].count_nonzero(axis=0)
-                n = len(X)
-                centroids[jc] = ( (nq/n) >= (nc/n) )
-    print("%d iterations,  cluster sizes:" % jiter, np.bincount(xtoc))
-    if verbose >= 2:
-        r50 = np.zeros(k)
-        r90 = np.zeros(k)
-        for j in range(k):
-            dist = Xdistances[xtoc==j]
-            if len(dist) > 0:
-                r50[j], r90[j] = np.percentile(dist, (50, 90))
-        print "Cluster 50% radius", r50.astype(int)
-        print "Cluster 90% radius", r90.astype(int)
-            # scale L1 / dim, L2 / sqrt(dim) ?
-    return centroids, xtoc, Xdistances
+    # ----------------------
+    # INIT [see Huang, 1998]
+    # ----------------------
+    
+    Xclust = np.zeros(N)
+    # determine frequencies of attributes
+    freqs = [0]*len(dim)
+    for idim in range(dim):
+        bc = np.bincount(X[:,idim])
+        inds = np.nonzero(bc)[0] # delete [0]?
+        sinds = np.argsort(inds)[::-1]
+        freqs[idim] = zip(inds[sinds], bc[sinds])
+    # now sample centroids using the probabilities of attributes
+    # (I assume that's what's meant in the paper)
+    for ik in range(k):
+        for idim in range(dim):
+            cent[ik, idim] = weighted_choice(freqs[idim])
+        # could result in empty clusters, so set centroid to closest point in X
+        minDist = 1e12
+        for iN in range(N):
+            dist = get_distance(X[iN], cent[ik])
+            # extra check here: we don't want 2 the same centroids
+            if dist < minDist and not (X[iN]==cent[:ik]).any():
+                minDist = dist
+                minInd = iN
+        cent[ik] = X[minInd]
+    
+    # ----------------------
+    # ITERATION
+    # ----------------------
+    # (dim, dim), because last one might have all unique attributes
+    freqClust = np.zeros(k, dim, dim)
+    iters = 0
+    converged = False
+    while iters <= maxiters and not converged:
+        iters += 1
+        moves = 0
+        for iN in range(N):
+            minDist = 1e12
+            for ik in range(k):
+                dist = get_distance(X[iN], cent[ik])
+                if dist < minDist:
+                    minDist = dist
+                    cluster = ik
+            # record initial frequencies of attributes in clusters
+            if iter == 1:
+                for idim in range(dim):
+                freqClust[cluster, idim, X[iN, idim]] += 1
+            
+            # if necessary, move point and update centroids
+            if Xclust[iN] ne cluster:
+                moves += 1
+                oldcluster = Xclust[iN]
+                for idim in range(dim):
+                    # update frequencies of attributes in clusters
+                    freqClust[cluster, idim, X[iN, idim]] += 1
+                    freqClust[oldcluster, idim, X[iN, idim]] -= 1
+                    # update the centroids by choosing the most likely attribute
+                    cent[cluster, idim] = freqClust[cluster, idim].argmax()
+                    cent[oldcluster, idim] = freqClust[oldcluster, idim].argmax()
+        converged = (moves == 0)
+        
+    return Xclust, cent
 
-#...............................................................................
-def kmodesinit( X, k, nsample=0, **kwargs ):
-    ''' 2-pass kmeans, fast for large N:
-        1) kmeans a random sample of nsample ~ sqrt(N) from X
-        2) full kmeans, starting from those centres
-    '''
-        # merge w kmeans ? mttiw
-        # v large N: sample N^1/2, N^1/2 of that
-        # seed like sklearn ?
-    N, dim = X.shape
-    if nsample == 0:
-        nsample = max( 2*np.sqrt(N), 10*k )
-    Xsample = randomsample( X, int(nsample) )
-    pass1centres = randomsample( X, int(k) )
-    samplecentres = kmeans( Xsample, pass1centres, **kwargs )[0]
-    return kmeans( X, samplecentres, **kwargs )
+def get_distance(A, B):
+    # simple matching
+    return (A!=B).sum()
 
-def get_distances(X, C):
-    D = np.array(X.shape[0], C.shape[0])
-    for ii in X.shape[0]:
-        for jj in C.shape[0]:
-            D[ii,jj] = (X[ii,:] != C[jj,:]).sum()
-    return D
+def weighted_choice(choices):
+    total = sum(w for c, w in choices)
+    r = np.random.uniform(0, total)
+    upto = 0
+    for c, w in choices:
+        if upto + w > r:
+            return c
+        upto += w
+    # shouldn't get this far
+    assert False
 
-def randomsample( X, n ):
-    ''' random.sample of the rows of X
-        X may be sparse -- best csr
-    '''
-    sampleix = random.sample( xrange( X.shape[0] ), int(n) )
-    return X[sampleix]
-
-def nearestcentres( X, centres, metric="euclidean", p=2 ):
-    ''' each X -> nearest centre, any metric
-            euclidean2 (~ withinss) is more sensitive to outliers,
-            cityblock (manhattan, L1) less sensitive
-    '''
-    D = cdist( X, centres, metric=metric, p=p )  # |X| x |centres|
-    return D.argmin(axis=1)
-
-def Lqmetric( x, y=None, q=.5 ):
-    # yes a metric, may increase weight of near matches; see ...
-    return (np.abs(x - y) ** q) .mean() if y is not None \
-        else (np.abs(x) ** q) .mean()
-
-#...............................................................................
-class Kmodes:
-    ''' km = Kmodes( X, k= or centroids=, ... )
-        in: either initial centroids= for kmodes
-            or k= [nsample=] for kmodesinit
-        out: km.centroids, km.Xtocentre, km.distances
-        iterator:
-            for jcentroid, J in km:
-                clustercentroid = centroids[jcentroid]
-                J indexes e.g. X[J], classes[J]
-    '''
-    def __init__( self, X, k=0, centroids=None, nsample=0, **kwargs ):
-        self.X = X
-        if centroids is None:
-            self.centroids, self.Xtocentroid, self.distances = kmodesinit(
-                X, k=k, nsample=nsample, **kwargs )
-        else:
-            self.centroids, self.Xtocentroid, self.distances = kmodes(
-                X, centroids, **kwargs )
-
-    def __iter__(self):
-        for jc in range(len(self.centroids)):
-            yield jc, (self.Xtocentroid == jc)
-
-#...............................................................................
 if __name__ == "__main__":
-    import random
-    import sys
-    from time import time
-
-    N = 10000
-    dim = 10
-    ncluster = 10
-    kmsample = 100  # 0: random centres, > 0: kmeanssample
-    kmdelta = .001
-    kmiter = 10
-    metric = "cityblock"  # "chebyshev" = max, "cityblock" L1,  Lqmetric
-    seed = 1
-
-    exec( "\n".join( sys.argv[1:] ))  # run this.py N= ...
-    np.set_printoptions( 1, threshold=200, edgeitems=5, suppress=True )
-    np.random.seed(seed)
-    random.seed(seed)
-
-    print "N %d  dim %d  ncluster %d  kmsample %d  metric %s" % (
-        N, dim, ncluster, kmsample, metric)
-    X = np.random.exponential( size=(N,dim) )
-        # cf scikits-learn datasets/
-    t0 = time()
-    if kmsample > 0:
-        centres, xtoc, dist = kmeanssample( X, ncluster, nsample=kmsample,
-            delta=kmdelta, maxiter=kmiter, metric=metric, verbose=2 )
-    else:
-        randomcentres = randomsample( X, ncluster )
-        centres, xtoc, dist = kmeans( X, randomcentres,
-            delta=kmdelta, maxiter=kmiter, metric=metric, verbose=2 )
-    print "%.0f msec" % ((time() - t0) * 1000)
+    pass
