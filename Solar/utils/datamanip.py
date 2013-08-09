faf9329742e4772f41e0cda64163a287f67bd767
nicodv <njdevos@gmail.com>
2013-08-09 15:20:53 +0200

    Update datamanip.py

--- Solar/utils/datamanip.py
+++ Solar/utils/datamanip.py
@@ -2,6 +2,7 @@ import os
 import json
 import pandas as pd
 import numpy as np
+import netCDF4
 
 with open('../SETTINGS.json') as data_file:
     basePath = json.load(data_file)['base_path']
@@ -17,3 +18,14 @@ def load_mesonet(fileName='train.csv'):
     path = os.path.join(basePath, fileName)
     mesoData = pd.read_csv(path, index_col=0, parse_dates=True, dtype='np.float64')
     return mesoData
+
+def load_gefs(dataset):
+    if dataset == 'train':
+        fileName = 'gefs_train.nc'
+    elif dataset == 'test':
+        fileName = 'gefs_test.nc'
+    else:
+        raise Exception("Unknown data set.")
+    path = os.path.join(basePath, fileName)
+    gefsData = netCDF4.Dataset(path)
+    return gefsData
