1aadbc19657bf9d692b76fe971cf528b7e06748e
nicodv <njdevos@gmail.com>
2013-08-09 10:11:21 +0200

    Update variogram.py

--- Solar/variogram.py
+++ Solar/variogram.py
@@ -115,10 +115,16 @@ def variogram(X, y, bins=20, maxDistFrac=0.5, subSample=1., thetaStep=30):
     gamma = accum_np(inds, yDist, func=varFunc,  fillVal=np.nan)
     nums = accum_np(inds, np.ones(yDist.shape), func=np.sum, fillVal=np.nan)
     
+    # necessary?
+    #gamma = gamma[:-1]
+    #num = nums[:-1]
+    
     return {'X': X,
             'y': y,
             'distance': XDist,
             'bindistance': distEdge[distInd] + tol/2,
+            'distbin': distEdge + tol/2,
+            'ydistance': yDist,
             'gamma': gamma,
             'theta': thetaCent[thetaInd],
             'bincount': nums
@@ -172,8 +178,8 @@ if __name__ == '__main__':
     y = np.random.rand(1000,1)*4 - 2
     z = 3*np.sin(x*15) + np.random.randn(len(x),1)
     varData = variogram(np.hstack((x, y)), z, bins=50, maxDistFrac=0.5, subSample=1., thetaStep=30)
-    dist, bdist, gamma, maxD, theta = varData['distance'], varData['bindistance'], varData['gamma'], \
-                                      varData['maxD'], varData['theta']
+    dist, bdist, bindist, ydist, gamma, maxD, theta = varData['distance'], varData['bindistance'], \
+            varData['distbin'], varData['ydistance'], varData['gamma'], varData['maxD'], varData['theta']
     
     fig = plt.figure(1)
     ax = fig.add_subplot(2, 3, 1)
@@ -189,25 +195,25 @@ if __name__ == '__main__':
     ax.set_title("Histogram of z-values")
     
     ax = fig.add_subplot(2, 3, 3)
-    plot_variogram(ax, bdist, gamma, maxD, cloud=True)
+    plot_variogram(ax, bdist, ydist, maxD, cloud=True)
     ax.set_title("Variogram cloud (binned distances)")
     ax = fig.add_subplot(2, 3, 4)
-    plot_variogram(ax, dist, gamma, maxD, cloud=True)
+    plot_variogram(ax, dist, ydist, maxD, cloud=True)
     ax.set_title("Variogram cloud (raw distances)")
     
     ax = fig.add_subplot(2, 3, 5)
-    plot_variogram(ax, bdist, gamma, maxD)
+    plot_variogram(ax, distbin, gamma, maxD)
     ax.set_title("Isotropic variogram")
     ax.grid()
     
     ax = fig.add_subplot(2, 3, 6, projection='3d')
-    plot_variogram(ax, bdist, gamma, maxD, theta)
+    plot_variogram(ax, distbin, gamma, maxD, theta)
     ax.set_title("Anisotropic variogram")
     
     fig = plt.figure(2)
     for ii in range(theta):
         ax = fig.add_subplot(2, 3, ii+1)
-        plot_variogram(ax, bdist, gamma, maxD, theta[ii])
+        plot_variogram(ax, distbin, gamma, maxD, theta[ii])
         ax.set_title("Variogram for theta = %f3. degrees" % ((theta[ii]/math.pi)*180, ))
     
     plt.show()
