c432b003746fbf78527b0dc500f202b2afdd75df
nicodv <njdevos@gmail.com>
2013-08-15 14:51:40 +0200

    Update analysis.py

--- Solar/analysis.py
+++ Solar/analysis.py
@@ -12,7 +12,9 @@ gefsData = np.apply_over_axes(np.mean, gefsData, [1, 2])
 
 fig = plt.figure(1)
 for ii, var in enumerate(variables):
-    varData = variogram.variogram(LAT EN LON GEFS METADATA, variable, thetaStep=15)
+    lats = gefsData.variables['lat'][:]
+    lons = gefsData.variables['lon'][:]
+    varData = variogram.variogram((lats, lons), variable, thetaStep=15)
     ax = fig.add_subplot(3, 5, ii+1, projection='3d')
     plot_variogram(ax, varData['distbin'], varData['gamma'], varData['maxD'], varData['thetabin'])
     ax.set_title("Anisotropic variogram for variable: {}".format(var))
@@ -26,6 +28,7 @@ plt.figure(); mesoData.plot(); plt.legend(loc='best')
 
 
 # SPATIAL DISPERSION
+import rpy2.robjects as robjects
 import itertools as iter
 from scipy.spatial.distance import euclidean
 
@@ -35,3 +38,6 @@ for curVar in ('temp', ):
     # spatial dispersion is defined as the variance of the difference in time series
     spatialDisp = [np.var(curData[x] - curData[y]) for a, b in iter.combinations(stations.columns, 2)]
     distances = [euclidean(stations[a], stations[b]) for a, b in iter.combinations(stations.columns, 2)]
+
+
+robjects.r('command')
