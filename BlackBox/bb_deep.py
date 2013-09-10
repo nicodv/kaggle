10ca9be06a7cbf976d28022fe6e7b902f3006bc1
Nico de Vos <njdevos@gmail.com>
2013-09-10 21:40:50 +0200

    cleanup

--- BlackBox/bb_deep.py
+++ BlackBox/bb_deep.py
@@ -26,241 +26,241 @@ from pylearn2.corruption import GaussianCorruptor, BinomialCorruptor
 DATA_DIR = '/home/nico/datasets/Kaggle/BlackBox/'
 
 class CAE_cost(costs.cost.Cost):
-    def __call__(self, model, X, Y=None, coef1=0.05, coef2=0, ** kwargs):
-        if coef1 and coef2:
-            cost = ((model.reconstruct(X) - X) ** 2).sum(axis=1).mean() + coef1*model.contraction_penalty(X) + coef2*model.higher_order_penalty(X)
-        elif coef1:
-            cost = ((model.reconstruct(X) - X) ** 2).sum(axis=1).mean() + coef1*model.contraction_penalty(X)
-        else:
-            cost = ((model.reconstruct(X) - X) ** 2).sum(axis=1).mean()
-        return cost
+	def __call__(self, model, X, Y=None, coef1=0.05, coef2=0, ** kwargs):
+		if coef1 and coef2:
+			cost = ((model.reconstruct(X) - X) ** 2).sum(axis=1).mean() + coef1*model.contraction_penalty(X) + coef2*model.higher_order_penalty(X)
+		elif coef1:
+			cost = ((model.reconstruct(X) - X) ** 2).sum(axis=1).mean() + coef1*model.contraction_penalty(X)
+		else:
+			cost = ((model.reconstruct(X) - X) ** 2).sum(axis=1).mean()
+		return cost
 
 class Rectify(object):
-    def __call__(self, X_before_activation):
-        # X_before_activation is linear inputs of hidden units, dense
-        return X_before_activation * (X_before_activation > 0)
+	def __call__(self, X_before_activation):
+		# X_before_activation is linear inputs of hidden units, dense
+		return X_before_activation * (X_before_activation > 0)
 
 def process_data():
-    # pre-process unsupervised data
-    if not os.path.exists(DATA_DIR+'preprocess.pkl') \
-    or not os.path.exists(DATA_DIR+'unsup_prep_data.pkl') \
-    or not os.path.exists(DATA_DIR+'sup_prep_data.pkl'):
-        unsup_data = black_box_dataset.BlackBoxDataset('extra')
-        pipeline = preprocessing.Pipeline()
-        pipeline.items.append(preprocessing.Standardize(global_mean=False, global_std=False))
-        #pipeline.items.append(preprocessing.ZCA(filter_bias=.1))
-        unsup_data.apply_preprocessor(preprocessor=pipeline, can_fit=True)
-        serial.save(DATA_DIR+'preprocess.pkl', pipeline)
-        
-        # why the hell do I get pickling errors if I use serial here? solve by pickling myself
-        #serial.save(DATA_DIR+'unsup_prep_data.pkl', unsup_data)
-        out = open(DATA_DIR+'unsup_prep_data.pkl', 'w')
-        pickle.dump(unsup_data, out)
-        out.close()
-        
-        # process supervised training data
-        sup_data = []
-        which_data = ['train']*3 + ['public_test']
-        starts = [0, 800, None, None]
-        stops = [800, 1000, None, None]
-        fits = [False, False, False, False]
-        for curstr, start, stop, fit in zip(which_data, starts, stops, fits):
-            sup_data.append(black_box_dataset.BlackBoxDataset(
-            which_set=curstr,
-            start=start,
-            stop=stop,
-            preprocessor=pipeline,
-            fit_preprocessor=fit
-            ))
-        serial.save(DATA_DIR+'sup_prep_data.pkl', sup_data)
-        
-    else:
-        pipeline = serial.load(DATA_DIR+'preprocess.pkl')
-        #unsup_data = serial.load(DATA_DIR+'unsup_prep_data.pkl')
-        unsup_data = pickle.load(open(DATA_DIR+'unsup_prep_data.pkl', 'r'))
-        sup_data = serial.load(DATA_DIR+'sup_prep_data.pkl')
-    
-    return unsup_data, sup_data
+	# pre-process unsupervised data
+	if not os.path.exists(DATA_DIR+'preprocess.pkl') \
+	or not os.path.exists(DATA_DIR+'unsup_prep_data.pkl') \
+	or not os.path.exists(DATA_DIR+'sup_prep_data.pkl'):
+		unsup_data = black_box_dataset.BlackBoxDataset('extra')
+		pipeline = preprocessing.Pipeline()
+		pipeline.items.append(preprocessing.Standardize(global_mean=False, global_std=False))
+		#pipeline.items.append(preprocessing.ZCA(filter_bias=.1))
+		unsup_data.apply_preprocessor(preprocessor=pipeline, can_fit=True)
+		serial.save(DATA_DIR+'preprocess.pkl', pipeline)
+		
+		# why the hell do I get pickling errors if I use serial here? solve by pickling myself
+		#serial.save(DATA_DIR+'unsup_prep_data.pkl', unsup_data)
+		out = open(DATA_DIR+'unsup_prep_data.pkl', 'w')
+		pickle.dump(unsup_data, out)
+		out.close()
+		
+		# process supervised training data
+		sup_data = []
+		which_data = ['train']*3 + ['public_test']
+		starts = [0, 800, None, None]
+		stops = [800, 1000, None, None]
+		fits = [False, False, False, False]
+		for curstr, start, stop, fit in zip(which_data, starts, stops, fits):
+			sup_data.append(black_box_dataset.BlackBoxDataset(
+			which_set=curstr,
+			start=start,
+			stop=stop,
+			preprocessor=pipeline,
+			fit_preprocessor=fit
+			))
+		serial.save(DATA_DIR+'sup_prep_data.pkl', sup_data)
+		
+	else:
+		pipeline = serial.load(DATA_DIR+'preprocess.pkl')
+		#unsup_data = serial.load(DATA_DIR+'unsup_prep_data.pkl')
+		unsup_data = pickle.load(open(DATA_DIR+'unsup_prep_data.pkl', 'r'))
+		sup_data = serial.load(DATA_DIR+'sup_prep_data.pkl')
+	
+	return unsup_data, sup_data
 
 def construct_ae(structure):
-    # some settings
-    irange = 0.05
-    
-    layers = []
-    for vsize,hsize in zip(structure[:-1], structure[1:]):
-        # DenoisingAutoencoder?, ContractiveAutoencoder?, HigherOrderContractiveAutoencoder?
-        layers.append(autoencoder.ContractiveAutoencoder(
-            # DenoisingAutoencoder
-            #corruptor=BinomialCorruptor(0.5),
-            # HigherOrderContractiveAutoencoder
-            #corruptor=GaussianCorruptor(0.5),
-            #num_corruptions=8,
-            nvis=vsize,
-            nhid=hsize,
-            tied_weights=True,
-            act_enc='sigmoid',
-            act_dec='sigmoid',
-            #act_enc=Rectify(),
-            #act_dec=Rectify(),
-            irange=irange
-        ))
-    return StackedBlocks(layers)
+	# some settings
+	irange = 0.05
+	
+	layers = []
+	for vsize,hsize in zip(structure[:-1], structure[1:]):
+		# DenoisingAutoencoder?, ContractiveAutoencoder?, HigherOrderContractiveAutoencoder?
+		layers.append(autoencoder.ContractiveAutoencoder(
+			# DenoisingAutoencoder
+			#corruptor=BinomialCorruptor(0.5),
+			# HigherOrderContractiveAutoencoder
+			#corruptor=GaussianCorruptor(0.5),
+			#num_corruptions=8,
+			nvis=vsize,
+			nhid=hsize,
+			tied_weights=True,
+			act_enc='sigmoid',
+			act_dec='sigmoid',
+			#act_enc=Rectify(),
+			#act_dec=Rectify(),
+			irange=irange
+		))
+	return StackedBlocks(layers)
 
 def get_ae_pretrainer(layer, data, batch_size):
-    init_lr = 0.1
-    dec_fac = 1.0001
-    
-    train_algo = sgd.SGD(
-        batch_size = batch_size,
-        learning_rate = init_lr,
-        init_momentum = 0.5,
-        monitoring_batches = 100/batch_size,
-        monitoring_dataset = {'train': data},
-        #cost = MeanSquaredReconstructionError(),
-        cost = CAE_cost(),
-        termination_criterion = EpochCounter(20),
-        update_callbacks = sgd.ExponentialDecay(decay_factor=dec_fac, min_lr=0.02)
-        )
-    return Train(model=layer, algorithm=train_algo, dataset=data, \
-            extensions=[sgd.MomentumAdjustor(final_momentum=0.9, start=0, saturate=15), ])
+	init_lr = 0.1
+	dec_fac = 1.0001
+	
+	train_algo = sgd.SGD(
+		batch_size = batch_size,
+		learning_rate = init_lr,
+		init_momentum = 0.5,
+		monitoring_batches = 100/batch_size,
+		monitoring_dataset = {'train': data},
+		#cost = MeanSquaredReconstructionError(),
+		cost = CAE_cost(),
+		termination_criterion = EpochCounter(20),
+		update_callbacks = sgd.ExponentialDecay(decay_factor=dec_fac, min_lr=0.02)
+		)
+	return Train(model=layer, algorithm=train_algo, dataset=data, \
+			extensions=[sgd.MomentumAdjustor(final_momentum=0.9, start=0, saturate=15), ])
 
 def construct_dbn_from_stack(stack):
-    # some settings
-    irange = 0.05
-    
-    layers = []
-    for ii, layer in enumerate(stack.layers()):
-        lr_scale = 0.25 if ii==0 else 0.25
-        layers.append(mlp.Sigmoid(
-            dim=layer.nhid,
-            layer_name='h'+str(ii),
-            irange=irange,
-            W_lr_scale=lr_scale,
-            max_col_norm=2.
-        ))
-    # softmax layer at then end for classification
-    layers.append(mlp.Softmax(
-        n_classes=9,
-        layer_name='y',
-        irange=irange,
-        W_lr_scale=0.25
-    ))
-    dbn = mlp.MLP(layers=layers, nvis=stack.layers()[0].get_input_space().dim)
-    # copy weigths to DBN
-    for ii, layer in enumerate(stack.layers()):
-        dbn.layers[ii].set_weights(layer.get_weights())
-        dbn.layers[ii].set_biases(layer.hidbias.get_value(borrow=False))
-    return dbn
+	# some settings
+	irange = 0.05
+	
+	layers = []
+	for ii, layer in enumerate(stack.layers()):
+		lr_scale = 0.25 if ii==0 else 0.25
+		layers.append(mlp.Sigmoid(
+			dim=layer.nhid,
+			layer_name='h'+str(ii),
+			irange=irange,
+			W_lr_scale=lr_scale,
+			max_col_norm=2.
+		))
+	# softmax layer at then end for classification
+	layers.append(mlp.Softmax(
+		n_classes=9,
+		layer_name='y',
+		irange=irange,
+		W_lr_scale=0.25
+	))
+	dbn = mlp.MLP(layers=layers, nvis=stack.layers()[0].get_input_space().dim)
+	# copy weigths to DBN
+	for ii, layer in enumerate(stack.layers()):
+		dbn.layers[ii].set_weights(layer.get_weights())
+		dbn.layers[ii].set_biases(layer.hidbias.get_value(borrow=False))
+	return dbn
 
 def get_finetuner(model, cost, trainset, validset=None, batch_size=100, iters=100):
-    train_algo = sgd.SGD(
-        batch_size = batch_size,
-        init_momentum = 0.5,
-        learning_rate = 0.5,
-        #monitoring_batches = 100/batch_size,
-        #monitoring_dataset = {'train': trainset, 'valid': validset},
-        cost = cost,
-        termination_criterion = EpochCounter(iters),
-        update_callbacks = sgd.ExponentialDecay(decay_factor=1.005, min_lr=0.05)
-    )
-    return Train(model=model, algorithm=train_algo, dataset=trainset, save_freq=0, \
-            extensions=[sgd.MomentumAdjustor(final_momentum=0.9, start=0, saturate=int(0.8*iters)), ])
+	train_algo = sgd.SGD(
+		batch_size = batch_size,
+		init_momentum = 0.5,
+		learning_rate = 0.5,
+		#monitoring_batches = 100/batch_size,
+		#monitoring_dataset = {'train': trainset, 'valid': validset},
+		cost = cost,
+		termination_criterion = EpochCounter(iters),
+		update_callbacks = sgd.ExponentialDecay(decay_factor=1.005, min_lr=0.05)
+	)
+	return Train(model=model, algorithm=train_algo, dataset=trainset, save_freq=0, \
+			extensions=[sgd.MomentumAdjustor(final_momentum=0.9, start=0, saturate=int(0.8*iters)), ])
 
 def get_output(model, data, batch_size):
-    model.set_batch_size(batch_size)
-    # dataset must be multiple of batch size of some batches will have
-    # different sizes. theano convolution requires a hard-coded batch size
-    m = data.X.shape[0]
-    extra = batch_size - m % batch_size
-    assert (m + extra) % batch_size == 0
-    if extra > 0:
-        data.X = np.concatenate((data.X, np.zeros((extra, data.X.shape[1]), dtype=data.X.dtype)), axis=0)
-    assert data.X.shape[0] % batch_size == 0
-    
-    Xb = model.get_input_space().make_theano_batch()
-    Yb = model.fprop(Xb)
-    y = T.argmax(Yb, axis=1)
-    f = function([Xb], y)
-    
-    y = []
-    for ii in xrange(data.X.shape[0] // batch_size):
-        x_arg = data.X[ii*batch_size:(ii+1)*batch_size,:]
-        if Xb.ndim > 2:
-            x_arg = data.get_topological_view(x_arg)
-        y.append(f(x_arg.astype(Xb.dtype)))
-    
-    y = np.concatenate(y)
-    assert y.ndim == 1
-    assert y.shape[0] == data.X.shape[0]
-    # discard any zero-padding that was used to give the batches uniform size
-    y = y[:m]
-    data.X = data.X[:m]
-    
-    return y
+	model.set_batch_size(batch_size)
+	# dataset must be multiple of batch size of some batches will have
+	# different sizes. theano convolution requires a hard-coded batch size
+	m = data.X.shape[0]
+	extra = batch_size - m % batch_size
+	assert (m + extra) % batch_size == 0
+	if extra > 0:
+		data.X = np.concatenate((data.X, np.zeros((extra, data.X.shape[1]), dtype=data.X.dtype)), axis=0)
+	assert data.X.shape[0] % batch_size == 0
+	
+	Xb = model.get_input_space().make_theano_batch()
+	Yb = model.fprop(Xb)
+	y = T.argmax(Yb, axis=1)
+	f = function([Xb], y)
+	
+	y = []
+	for ii in xrange(data.X.shape[0] // batch_size):
+		x_arg = data.X[ii*batch_size:(ii+1)*batch_size,:]
+		if Xb.ndim > 2:
+			x_arg = data.get_topological_view(x_arg)
+		y.append(f(x_arg.astype(Xb.dtype)))
+	
+	y = np.concatenate(y)
+	assert y.ndim == 1
+	assert y.shape[0] == data.X.shape[0]
+	# discard any zero-padding that was used to give the batches uniform size
+	y = y[:m]
+	data.X = data.X[:m]
+	
+	return y
 
 if __name__ == '__main__':
-    
-    # some settings
-    submission = True
-    # note: MSE van CAE gaat omhoog bij 4e layer, maar bij 5e layer enorm omlaag (?)
-    structure = [1875, 2000, 2000, 2000, 2000, 2000, 2000]
-    batch_size = 50
-    
-    unsup_data, sup_data = process_data()
-    
-    stack = serial.load(DATA_DIR+'cae6_005_pretrained.pkl')
-    #stack = construct_ae(structure)
-    
-    # pre-train model
-    #for ii, layer in enumerate(stack.layers()):
-        #utraindata = unsup_data if ii==0 else TransformerDataset(raw=unsup_data,
-        #                                        transformer=StackedBlocks(stack.layers()[:ii]))
-        #pretrainer = get_ae_pretrainer(layer, utraindata, batch_size)
-        #pretrainer.main_loop()
-    
-    #serial.save(DATA_DIR+'cae6_005_pretrained.pkl', stack)
-    
-    # construct DBN
-    dbn = construct_dbn_from_stack(stack)
-    
-    # train DBN
-    if submission:
-        traindata = sup_data[2]
-        validdata = sup_data[2]
-    else:
-        traindata = sup_data[0]
-        validdata = sup_data[1]
-    
-    cost = dropout.Dropout(input_include_probs={'h0': 0.5}, input_scales={'h0': 1./0.5},
-                           default_input_include_prob=0.5, default_input_scale=1./0.5)
-    
-    # finetune softmax layer a bit
-    finetuner = get_finetuner(dbn, cost, traindata, validdata, batch_size, iters=100)
-    finetuner.main_loop()
-    
-    # now finetune layer-by-layer
-    lrs = [5., 2., 1., 0.5, 0.25]
-    for ii, lr in zip(range(len(structure)-1), lrs):
-        # set lr to boosted value for current layer
-        dbn.layers[ii].W_lr_scale = lr
-        
-        finetuner = get_finetuner(dbn, cost, traindata, validdata, batch_size, iters=50)
-        finetuner.main_loop()
-        
-        # return to original lr
-        dbn.layers[ii].W_lr_scale = 0.25
-    
-    # total finetuner
-    finetuner = get_finetuner(dbn, cost, traindata, validdata, batch_size, iters=200)
-    finetuner.main_loop()
-    
-    if submission:
-        # get output
-        output = get_output(dbn, sup_data[3], batch_size)
-        
-        # create submission
-        out = open(DATA_DIR+'submission.csv', 'w')
-        for i in xrange(output.shape[0]):
-            out.write('%d.0\n' % (output[i] + 1))
-        out.close()
-    
+	
+	# some settings
+	submission = True
+	# note: MSE van CAE gaat omhoog bij 4e layer, maar bij 5e layer enorm omlaag (?)
+	structure = [1875, 2000, 2000, 2000, 2000, 2000, 2000]
+	batch_size = 50
+	
+	unsup_data, sup_data = process_data()
+	
+	stack = serial.load(DATA_DIR+'cae6_005_pretrained.pkl')
+	#stack = construct_ae(structure)
+	
+	# pre-train model
+	#for ii, layer in enumerate(stack.layers()):
+		#utraindata = unsup_data if ii==0 else TransformerDataset(raw=unsup_data,
+		#                                        transformer=StackedBlocks(stack.layers()[:ii]))
+		#pretrainer = get_ae_pretrainer(layer, utraindata, batch_size)
+		#pretrainer.main_loop()
+	
+	#serial.save(DATA_DIR+'cae6_005_pretrained.pkl', stack)
+	
+	# construct DBN
+	dbn = construct_dbn_from_stack(stack)
+	
+	# train DBN
+	if submission:
+		traindata = sup_data[2]
+		validdata = sup_data[2]
+	else:
+		traindata = sup_data[0]
+		validdata = sup_data[1]
+	
+	cost = dropout.Dropout(input_include_probs={'h0': 0.5}, input_scales={'h0': 1./0.5},
+						   default_input_include_prob=0.5, default_input_scale=1./0.5)
+	
+	# finetune softmax layer a bit
+	finetuner = get_finetuner(dbn, cost, traindata, validdata, batch_size, iters=100)
+	finetuner.main_loop()
+	
+	# now finetune layer-by-layer
+	lrs = [5., 2., 1., 0.5, 0.25]
+	for ii, lr in zip(range(len(structure)-1), lrs):
+		# set lr to boosted value for current layer
+		dbn.layers[ii].W_lr_scale = lr
+		
+		finetuner = get_finetuner(dbn, cost, traindata, validdata, batch_size, iters=50)
+		finetuner.main_loop()
+		
+		# return to original lr
+		dbn.layers[ii].W_lr_scale = 0.25
+	
+	# total finetuner
+	finetuner = get_finetuner(dbn, cost, traindata, validdata, batch_size, iters=200)
+	finetuner.main_loop()
+	
+	if submission:
+		# get output
+		output = get_output(dbn, sup_data[3], batch_size)
+		
+		# create submission
+		out = open(DATA_DIR+'submission.csv', 'w')
+		for i in xrange(output.shape[0]):
+			out.write('%d.0\n' % (output[i] + 1))
+		out.close()
+	
--- BlackBox/black_box_dataset.py
+++ BlackBox/black_box_dataset.py
@@ -18,130 +18,130 @@ from pylearn2.utils import serial
 from pylearn2.utils.string_utils import preprocess
 
 class BlackBoxDataset(DenseDesignMatrix):
-    """
-    A Pylearn2 Dataset class for accessing the data for the
-    facial expression recognition Kaggle contest for the ICML
-    2013 workshop on representation learning.
-    """
+	"""
+	A Pylearn2 Dataset class for accessing the data for the
+	facial expression recognition Kaggle contest for the ICML
+	2013 workshop on representation learning.
+	"""
 
-    def __init__(self, which_set,
-            base_path = '/home/nico/datasets/Kaggle/BlackBox/',
-            start = None,
-            stop = None,
-            preprocessor = None,
-            fit_preprocessor = False,
-            fit_test_preprocessor = False):
-        """
-        which_set: A string specifying which portion of the dataset
-            to load. Valid values are 'train' or 'public_test'
-        base_path: The directory containing the .csv files from kaggle.com.
-                This directory should be writable; if the .csv files haven't
-                already been converted to npy, this class will convert them
-                to save memory the next time they are loaded.
-        fit_preprocessor: True if the preprocessor is allowed to fit the
-                   data.
-        fit_test_preprocessor: If we construct a test set based on this
-                    dataset, should it be allowed to fit the test set?
-        """
+	def __init__(self, which_set,
+			base_path = '/home/nico/datasets/Kaggle/BlackBox/',
+			start = None,
+			stop = None,
+			preprocessor = None,
+			fit_preprocessor = False,
+			fit_test_preprocessor = False):
+		"""
+		which_set: A string specifying which portion of the dataset
+			to load. Valid values are 'train' or 'public_test'
+		base_path: The directory containing the .csv files from kaggle.com.
+				This directory should be writable; if the .csv files haven't
+				already been converted to npy, this class will convert them
+				to save memory the next time they are loaded.
+		fit_preprocessor: True if the preprocessor is allowed to fit the
+				   data.
+		fit_test_preprocessor: If we construct a test set based on this
+					dataset, should it be allowed to fit the test set?
+		"""
 
-        self.test_args = locals()
-        self.test_args['which_set'] = 'public_test'
-        self.test_args['fit_preprocessor'] = fit_test_preprocessor
-        del self.test_args['start']
-        del self.test_args['stop']
-        del self.test_args['self']
+		self.test_args = locals()
+		self.test_args['which_set'] = 'public_test'
+		self.test_args['fit_preprocessor'] = fit_test_preprocessor
+		del self.test_args['start']
+		del self.test_args['stop']
+		del self.test_args['self']
 
-        files = {'train': 'train.csv', 'public_test' : 'test.csv'}
+		files = {'train': 'train.csv', 'public_test' : 'test.csv'}
 
-        if which_set == 'extra':
-            path = base_path + '/' + 'extra_unsupervised_data.npy'
-            X = serial.load(path).T
-            y = None
-        else:
-            try:
-                filename = files[which_set]
-            except KeyError:
-                raise ValueError("Unrecognized dataset name: " + which_set)
+		if which_set == 'extra':
+			path = base_path + '/' + 'extra_unsupervised_data.npy'
+			X = serial.load(path).T
+			y = None
+		else:
+			try:
+				filename = files[which_set]
+			except KeyError:
+				raise ValueError("Unrecognized dataset name: " + which_set)
 
-            path = base_path + '/' + filename
+			path = base_path + '/' + filename
 
-            path = preprocess(path)
+			path = preprocess(path)
 
-            X, y = self._load_data(path, which_set == 'train')
-
-
-        if start is not None:
-            assert which_set != 'test'
-            assert isinstance(start, int)
-            assert isinstance(stop, int)
-            assert start >= 0
-            assert start < stop
-            assert stop <= X.shape[0]
-            X = X[start:stop, :]
-            if y is not None:
-                y = y[start:stop, :]
+			X, y = self._load_data(path, which_set == 'train')
 
 
-        super(BlackBoxDataset, self).__init__(X=X, y=y)
+		if start is not None:
+			assert which_set != 'test'
+			assert isinstance(start, int)
+			assert isinstance(stop, int)
+			assert start >= 0
+			assert start < stop
+			assert stop <= X.shape[0]
+			X = X[start:stop, :]
+			if y is not None:
+				y = y[start:stop, :]
 
-        if preprocessor:
-            preprocessor.apply(self, can_fit=fit_preprocessor)
 
-    def adjust_for_viewer(self, X):
-        return (X - 127.5) / 127.5
+		super(BlackBoxDataset, self).__init__(X=X, y=y)
 
-    def get_test_set(self):
-        return BlackBoxDataset(**self.test_args)
+		if preprocessor:
+			preprocessor.apply(self, can_fit=fit_preprocessor)
 
-    def _load_data(self, path, expect_labels):
+	def adjust_for_viewer(self, X):
+		return (X - 127.5) / 127.5
 
-        assert path.endswith('.csv')
+	def get_test_set(self):
+		return BlackBoxDataset(**self.test_args)
 
-        # If a previous call to this method has already converted
-        # the data to numpy format, load the numpy directly
-        X_path = path[:-4] + '.X.npy'
-        Y_path = path[:-4] + '.Y.npy'
-        if os.path.exists(X_path):
-            X = np.load(X_path)
-            if expect_labels:
-                y = np.load(Y_path)
-            else:
-                y = None
-            return X, y
+	def _load_data(self, path, expect_labels):
 
-        # Convert the .csv file to numpy
-        csv_file = open(path, 'r')
+		assert path.endswith('.csv')
 
-        reader = csv.reader(csv_file)
+		# If a previous call to this method has already converted
+		# the data to numpy format, load the numpy directly
+		X_path = path[:-4] + '.X.npy'
+		Y_path = path[:-4] + '.Y.npy'
+		if os.path.exists(X_path):
+			X = np.load(X_path)
+			if expect_labels:
+				y = np.load(Y_path)
+			else:
+				y = None
+			return X, y
 
-        # Discard header
-        row = reader.next()
+		# Convert the .csv file to numpy
+		csv_file = open(path, 'r')
 
-        y_list = []
-        X_list = []
+		reader = csv.reader(csv_file)
 
-        for row in reader:
-            if expect_labels:
-                y_str = row[0]
-                row = row[1:]
-                y = int(float(y_str))
-                y_list.append(y)
-            X_row = map(lambda x: float(x), row)
-            X_list.append(X_row)
+		# Discard header
+		row = reader.next()
 
-        X = np.asarray(X_list).astype('float32')
-        if expect_labels:
-            y = np.asarray(y_list)
+		y_list = []
+		X_list = []
 
-            one_hot = np.zeros((y.shape[0],9),dtype='float32')
-            for i in xrange(y.shape[0]):
-                one_hot[i,y[i] - 1] = 1.
-            y = one_hot
-        else:
-            y = None
+		for row in reader:
+			if expect_labels:
+				y_str = row[0]
+				row = row[1:]
+				y = int(float(y_str))
+				y_list.append(y)
+			X_row = map(lambda x: float(x), row)
+			X_list.append(X_row)
 
-        np.save(X_path, X)
-        if y is not None:
-            np.save(Y_path, y)
+		X = np.asarray(X_list).astype('float32')
+		if expect_labels:
+			y = np.asarray(y_list)
 
-        return X, y
+			one_hot = np.zeros((y.shape[0],9),dtype='float32')
+			for i in xrange(y.shape[0]):
+				one_hot[i,y[i] - 1] = 1.
+			y = one_hot
+		else:
+			y = None
+
+		np.save(X_path, X)
+		if y is not None:
+			np.save(Y_path, y)
+
+		return X, y
